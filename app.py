4f739997add063d9fe3237746bfcb1857ffb21d0
maggyy666 <kczyz20@gmail.com>
2024-05-23 23:20:44 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    use_custom_background = True
+    return render_template('index.html',use_custom_background=use_custom_background)
+@app.route('/second')
+def second():
+    article = request.args.get('article')
+    return render_template('second.html', article=article)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
