c269747eb2c31acd26b215aceb57dc2e30f69f70
maggyy666 <kczyz20@gmail.com>
2024-05-30 11:08:56 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def index():
     use_custom_background = True
     return render_template('index.html',use_custom_background=use_custom_background)
-@app.route('/second')
+@app.route('/articles')
 def second():
     article = request.args.get('article')
-    return render_template('second.html', article=article)
-    
+    return render_template('articles.html', article=article)
+
 if __name__ == "__main__":
     app.run(debug=True)
